0177fb4416a21ce469329ae4d41863ad97c8dbfb
sumit <sumitsongoriya15@gmail.com>
2024-11-09 15:20:32 +0530

    4

--- script.js
+++ script.js
@@ -27,86 +27,102 @@ function loco() {
 
     // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
-}
 
+}
 loco()
+
 
 gsap.to("#page>video", {
     scrollTrigger: {
-        trigger: '#page>video',
-        start: '2% top',
-        end: 'bottom top',
-        scroller: '#main'
+        trigger: `#page>video`,
+        start: `2% top`,
+        end: `bottom top`,
+        scroller: `#main`
     },
     onStart: () => {
         document.querySelector("#page>video").play()
-    },
+    }
 })
+
 
 gsap.to("#page", {
     scrollTrigger: {
-        trigger: '#page',
-        start: 'top top',
-        end: 'bottom top',
-        scroller: '#main',
+        trigger: `#page`,
+        start: `top top`,
+        end: `bottom top`,
+        scroller: `#main`,
         pin: true
     }
 })
+
+
 gsap.to("#page-bottom", {
     scrollTrigger: {
-        trigger: '#page-bottom',
-        start: '5% top',
-        end: 'bottom top',
-        scroller: '#main',
+        trigger: `#page-bottom`,
+        start: `5% top`,
+        end: `bottom top`,
+        scroller: `#main`,
+        scrub: .5,
     },
     opacity: 0
 })
 
+
+
 var tl = gsap.timeline({
     scrollTrigger: {
-        trigger: '#page1',
-        start: 'top top',
+        trigger: `#page1`,
+        start: `top top`,
         scrub: 1,
-        scroller: '#main',
-        // markers: true,
+        scroller: `#main`,
         pin: true
     }
 })
 
+
 tl.to("#page1>h1", {
-    top: '-50%'
+    top: `-50%`
 })
+
+
+
+
 
 
 var tl1 = gsap.timeline({
     scrollTrigger: {
-        trigger: '#page2',
-        start: 'top top',
+        trigger: `#page2`,
+        start: `top top`,
         scrub: 1,
-        scroller: '#main',
-        // markers: true,
+        scroller: `#main`,
         pin: true
     }
 })
 
+
 tl1.to("#page2>h1", {
-    top: '-50%'
+    top: `-50%`
 })
+
+
+
 
 var tl2 = gsap.timeline({
     scrollTrigger: {
-        trigger: '#page4',
-        start: 'top top',
+        trigger: `#page4`,
+        start: `top top`,
         scrub: 1,
-        scroller: '#main',
-        // markers: true,
+        scroller: `#main`,
         pin: true
     }
 })
 
+
 tl2.to("#page4>#center-page4", {
-    top: '-50%'
+    top: `-50%`
 })
+
+
 
 function canvas() {
     const canvas = document.querySelector("#page7>canvas");
@@ -124,6 +140,7 @@ function canvas() {
 
     function files(index) {
         var data = `
+  https://www.apple.com/105/media/us/apple-vision-pro/2023/7e268c13-eb22-493d-a860-f0637bacb569/anim/360/large/0000.jpg
   https://www.apple.com/105/media/us/apple-vision-pro/2023/7e268c13-eb22-493d-a860-f0637bacb569/anim/360/large/0001.jpg
   https://www.apple.com/105/media/us/apple-vision-pro/2023/7e268c13-eb22-493d-a860-f0637bacb569/anim/360/large/0002.jpg
   https://www.apple.com/105/media/us/apple-vision-pro/2023/7e268c13-eb22-493d-a860-f0637bacb569/anim/360/large/0003.jpg
@@ -326,7 +343,7 @@ function canvas() {
         return data.split("\n")[index];
     }
 
-    const frameCount = 199;
+    const frameCount = 198;
 
     const images = [];
     const imageSeq = {
@@ -345,7 +362,7 @@ function canvas() {
         ease: `none`,
         scrollTrigger: {
             scrub: 0.15,
-            trigger: `canvas`,
+            trigger: `#page7>canvas`,
             //   set start end according to preference
             start: `top top`,
             end: `600% top`,
@@ -382,7 +399,7 @@ function canvas() {
     }
     ScrollTrigger.create({
 
-        trigger: "canvas",
+        trigger: "#page7>canvas",
         pin: true,
         // markers:true,
         scroller: `#main`,
@@ -391,5 +408,163 @@ function canvas() {
         end: `600% top`,
     });
 }
-
 canvas();
+
+
+
+function canvas1() {
+    const canvas = document.querySelector("#page18>canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+    window.addEventListener("resize", function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        render();
+    });
+
+    function files(index) {
+        var data = `
+.//Apple vision canvas images/Vision00001.png
+.//Apple vision canvas images/Vision00002.png
+.//Apple vision canvas images/Vision00003.png
+.//Apple vision canvas images/Vision00004.png
+.//Apple vision canvas images/Vision00005.png
+.//Apple vision canvas images/Vision00006.png
+.//Apple vision canvas images/Vision00007.png
+.//Apple vision canvas images/Vision00008.png
+.//Apple vision canvas images/Vision00009.png
+.//Apple vision canvas images/Vision00010.png
+.//Apple vision canvas images/Vision00011.png
+.//Apple vision canvas images/Vision00012.png
+.//Apple vision canvas images/Vision00013.png
+.//Apple vision canvas images/Vision00014.png
+.//Apple vision canvas images/Vision00015.png
+.//Apple vision canvas images/Vision00016.png
+.//Apple vision canvas images/Vision00017.png
+.//Apple vision canvas images/Vision00018.png
+.//Apple vision canvas images/Vision00019.png
+.//Apple vision canvas images/Vision00020.png
+.//Apple vision canvas images/Vision00021.png
+.//Apple vision canvas images/Vision00022.png
+.//Apple vision canvas images/Vision00023.png
+.//Apple vision canvas images/Vision00024.png
+.//Apple vision canvas images/Vision00025.png
+`;
+        return data.split("\n")[index];
+    }
+
+    const frameCount = 25;
+
+    const images = [];
+    const imageSeq = {
+        frame: 1,
+    };
+
+    for (let i = 0; i < frameCount; i++) {
+        const img = new Image();
+        img.src = files(i);
+        images.push(img);
+    }
+
+    gsap.to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: `none`,
+        scrollTrigger: {
+            scrub: 0.15,
+            trigger: `#page18`,
+            //   set start end according to preference
+            start: `top top`,
+            end: `80% top`,
+            scroller: `#main`,
+        },
+        onUpdate: render,
+    });
+
+    images[1].onload = render;
+
+    function render() {
+        scaleImage(images[imageSeq.frame], context);
+    }
+
+    function scaleImage(img, ctx) {
+        var canvas = ctx.canvas;
+        var hRatio = canvas.width / img.width;
+        var vRatio = canvas.height / img.height;
+        var ratio = Math.max(hRatio, vRatio);
+        var centerShift_x = (canvas.width - img.width * ratio) / 2;
+        var centerShift_y = (canvas.height - img.height * ratio) / 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(
+            img,
+            0,
+            0,
+            img.width,
+            img.height,
+            centerShift_x,
+            centerShift_y,
+            img.width * ratio,
+            img.height * ratio
+        );
+    }
+    ScrollTrigger.create({
+
+        trigger: "#page18",
+        pin: true,
+        // markers:true,
+        scroller: `#main`,
+        //   set start end according to preference
+        start: `top top`,
+        end: `80% top`,
+    });
+}
+canvas1();
+
+
+
+var tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: `#page21`,
+        start: `top top`,
+        scrub: 1,
+        scroller: `#main`,
+        pin: true
+    }
+})
+
+
+tl3.to("#page21>#troff", {
+    opacity: 0
+})
+
+var tl4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: `#page22`,
+        start: `top top`,
+        scrub: 1,
+        scroller: `#main`,
+        pin: true
+    }
+})
+
+
+tl4.to("#page22>#snroff", {
+    opacity: 0
+})
+
+
+
+gsap.to("#page23>img", {
+    scrollTrigger: {
+        trigger: `#page23>img`,
+        start: `top bottom`,
+        end: `bottom 60%`,
+        scrub: .5,
+        scroller: `#main`
+    },
+    opacity: 1
+})
